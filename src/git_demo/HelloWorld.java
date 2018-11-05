9f08a312cdfd6f35f4a00820caa2bff4419a5817
niminghui <635681077@qq.com>
2018-11-05 10:35:21 +0800

    首次提交到本地仓库

---
+++ src/git_demo/HelloWorld.java
@@ -0,0 +1,33 @@
+/**
+ * Project Name:git_demo
+ * File Name:HelloWorld.java
+ * Package Name:git_demo
+ * Date:2018年11月5日上午10:31:11
+ * Copyright (c) 2018, Minghui.Ni@sc.com All Rights Reserved.
+ *
+ */
+package git_demo;
+
+/**
+ * ClassName: HelloWorld <br/>
+ * Description: TODO ADD REASON(可选). <br/>
+ * <br/>
+ *
+ * @author Minghui.Ni
+ * @version V1.0
+ * @since JDK 1.8 date: 2018年11月5日 上午10:31:11 <br/>
+ */
+public class HelloWorld {
+
+	/**
+	 * EGit demo.<br/>
+	 * 
+	 * @param
+	 * @return
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
